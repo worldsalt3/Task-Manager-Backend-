a10c59941d6753577804e98858b751d6868115d8
Riliwan Hassan <40924439+worldsalt3@users.noreply.github.com>
2024-09-05 22:55:37 +0100

    implemented CRUD operation for the taask manager

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
 
+app.use(express.static("./public"));
+
 app.use(express.json());
 
 app.use("/api/v1/tasks", tasks);
