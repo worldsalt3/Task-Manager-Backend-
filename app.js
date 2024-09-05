9e93b9340749ef434a179d6b1137bc1f8d6cce32
Riliwan Hassan <40924439+worldsalt3@users.noreply.github.com>
2024-09-05 22:14:51 +0100

    implemented CRUD operation for the taask manager

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
+require("dotenv").config();
 
 app.use(express.json());
-
-// routes
 
 app.use("/api/v1/tasks", tasks);
 
@@ -13,7 +12,7 @@ const port = 3000;
 
 const start = async () => {
   try {
-    await connectDB();
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () => {
       console.log(`Listening on port ${port}.........`);
     });
--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 
-const connectionString =
-  "mongodb+srv://riliwanhazzan:fwDTNNQa0aPH2BF1@cluster0.s6eee.mongodb.net/TASK_MANAGER?retryWrites=true&w=majority";
-
 const connectDB = (url) => {
-  return mongoose.connect(connectionString, {
+  return mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
